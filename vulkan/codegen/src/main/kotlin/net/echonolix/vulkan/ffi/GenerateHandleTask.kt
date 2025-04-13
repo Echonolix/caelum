5fead2b3fface6ee580f0590aeb0206459e8a55e
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-04-13 16:50:03 -0400

    Gen Handle code cleanup

--- vulkan/codegen/src/main/kotlin/net/echonolix/vulkan/ffi/GenerateHandleTask.kt
+++ vulkan/codegen/src/main/kotlin/net/echonolix/vulkan/ffi/GenerateHandleTask.kt
@@ -5,17 +5,18 @@ import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import net.echonolix.ktffi.*
 
 class GenerateHandleTask(ctx: VKFFICodeGenContext) : VKFFITask<Unit>(ctx) {
-    fun CType.Handle.variableName(): String {
+    private fun CType.Handle.variableName(): String {
         return name.removePrefix("Vk").decap()
     }
+
+    private val objTypeCname = with(ctx) { resolveType("VkObjectType").className() }
+    private val typeVariable = TypeVariableName("T", VKFFI.vkHandleCname)
+    val vkTypeDescriptorCname = VKFFI.vkHandleCname.nestedClass("TypeDescriptor")
 
     override fun VKFFICodeGenContext.compute() {
         val handles = ctx.filterType<CType.Handle>()
         val typeAlias = GenTypeAliasTask(this, handles).fork()
-        val vkTypeDescriptorCname = VKFFI.vkHandleCname.nestedClass("TypeDescriptor")
 
-        val objTypeCname = resolveType("VkObjectType").className()
-        val typeVariable = TypeVariableName("T", VKFFI.vkHandleCname)
         val vkHandleFile = FileSpec.builder(VKFFI.vkHandleCname)
             .addType(
                 TypeSpec.interfaceBuilder(VKFFI.vkHandleCname)
@@ -32,143 +33,143 @@ class GenerateHandleTask(ctx: VKFFICodeGenContext) : VKFFITask<Unit>(ctx) {
                     )
                     .build()
             )
+
         ctx.writeOutput(vkHandleFile)
 
         handles.parallelStream()
             .filter { (name, dstType) -> name == dstType.name }
-            .map { (_, type) ->
-                type as VkHandle
-                val thisCname = type.className()
-                val thisTypeDescriptor = KTFFICodegenHelper.typeDescriptorCname.parameterizedBy(thisCname)
-                val parent = type.parent
-                FileSpec.builder(thisCname)
-                    .addType(
-                        TypeSpec.interfaceBuilder(thisCname)
-                            .addSuperinterface(VKFFI.vkHandleCname)
-                            .apply {
-                                parent?.className()?.nestedClass("Child")?.let {
-                                    addSuperinterface(it)
-                                }
-                            }
-                            .addProperty(
-                                PropertySpec.builder("objectType", objTypeCname)
-                                    .addModifiers(KModifier.OVERRIDE)
-                                    .getter(
-                                        FunSpec.getterBuilder()
-                                            .addStatement("return %T.%N", objTypeCname, type.objectTypeEnum.name)
-                                            .build()
-                                    )
-                                    .build()
-                            )
-                            .addProperty(
-                                PropertySpec.builder("typeDescriptor", thisTypeDescriptor)
-                                    .addModifiers(KModifier.OVERRIDE)
-                                    .getter(
-                                        FunSpec.getterBuilder()
-                                            .addStatement("return Companion")
-                                            .build()
-                                    )
-                                    .build()
-                            )
-                            .addType(
-                                TypeSpec.classBuilder("Impl")
-                                    .addSuperinterface(thisCname)
-                                    .apply {
-                                        if (type.parent != null) {
-                                            val parentVariableName = type.parent.variableName()
-                                            val parentCname = type.parent.className()
-                                            addProperty(
-                                                PropertySpec.builder(parentVariableName, parentCname)
-                                                    .addModifiers(KModifier.OVERRIDE)
-                                                    .initializer(CodeBlock.of(parentVariableName))
-                                                    .build()
-                                            )
-                                            addProperty(
-                                                PropertySpec.builder("handle", CBasicType.int64_t.kotlinTypeName)
-                                                    .addModifiers(KModifier.OVERRIDE)
-                                                    .initializer(CodeBlock.of("handle"))
-                                                    .build()
-                                            )
-                                            primaryConstructor(
-                                                FunSpec.constructorBuilder()
-                                                    .addParameter(parentVariableName, type.parent.className())
-                                                    .addParameter("handle", CBasicType.int64_t.kotlinTypeName)
-                                                    .build()
-                                            )
-                                            val grandparent = type.parent.parent
-                                            if (grandparent != null) {
-                                                addSuperinterface(
-                                                    grandparent.className().nestedClass("Child"),
-                                                    CodeBlock.of(parentVariableName)
-                                                )
-                                            }
-                                        } else {
-                                            addSuperclassConstructorParameter(
-                                                CodeBlock.of("%M", CBasicType.int64_t.valueLayoutMember)
-                                            )
-                                            addProperty(
-                                                PropertySpec.builder("handle", CBasicType.int64_t.kotlinTypeName)
-                                                    .addModifiers(KModifier.OVERRIDE)
-                                                    .initializer(CodeBlock.of("handle"))
-                                                    .build()
-                                            )
-                                            primaryConstructor(
-                                                FunSpec.constructorBuilder()
-                                                    .addParameter("handle", CBasicType.int64_t.kotlinTypeName)
-                                                    .build()
-                                            )
-                                        }
-                                    }
-                                    .build()
-                            )
-                            .addType(
-                                TypeSpec.interfaceBuilder("Child")
-                                    .apply {
-                                        type.parent?.className()?.nestedClass("Child")?.let {
-                                            addSuperinterface(it)
-                                        }
-                                    }
-                                    .addProperty(type.variableName(), thisCname)
-                                    .build()
-                            )
-                            .addType(
-                                TypeSpec.companionObjectBuilder()
-                                    .superclass(vkTypeDescriptorCname.parameterizedBy(thisCname))
-                                    .addFunction(
-                                        FunSpec.builder("toNativeData")
-                                            .addAnnotation(JvmStatic::class)
-                                            .addModifiers(KModifier.INLINE)
-                                            .addParameter("value", thisCname)
-                                            .returns(CBasicType.int64_t.kotlinTypeName)
-                                            .addStatement("return value.handle")
-                                            .build()
-                                    )
-                                    .addFunction(
-                                        FunSpec.builder("fromNativeData")
-                                            .addAnnotation(JvmStatic::class)
-                                            .addModifiers(KModifier.INLINE)
-                                            .addParameter("value", CBasicType.int64_t.kotlinTypeName)
-                                            .returns(thisCname)
-                                            .apply {
-                                                if (type.parent == null) {
-                                                    addStatement("return Impl(value)")
-                                                } else {
-                                                    addStatement(
-                                                        "throw %T()",
-                                                        UnsupportedOperationException::class
-                                                    )
-                                                }
-                                            }
-                                            .build()
-                                    )
-                                    .addMethodHandleFields()
-                                    .build()
-                            )
-                            .build()
-                    )
-            }
+            .map { (_, handleType) -> genHandle(handleType) }
             .forEach(ctx::writeOutput)
 
         typeAlias.joinAndWriteOutput(VKFFI.handlePackageName)
+    }
+
+    private fun VKFFICodeGenContext.genHandle(handleType: CType.Handle): FileSpec.Builder {
+        handleType as VkHandle
+        val thisCname = handleType.className()
+        val thisTypeDescriptor = KTFFICodegenHelper.typeDescriptorCname.parameterizedBy(thisCname)
+        val parent = handleType.parent
+
+        val interfaceType = TypeSpec.interfaceBuilder(thisCname)
+        interfaceType.addSuperinterface(VKFFI.vkHandleCname)
+        parent?.className()?.nestedClass("Child")?.let {
+            interfaceType.addSuperinterface(it)
+        }
+
+        interfaceType.addProperty(
+            PropertySpec.builder("objectType", objTypeCname)
+                .addModifiers(KModifier.OVERRIDE)
+                .getter(
+                    FunSpec.getterBuilder()
+                        .addStatement("return %T.%N", objTypeCname, handleType.objectTypeEnum.name)
+                        .build()
+                )
+                .build()
+        )
+        interfaceType.addProperty(
+            PropertySpec.builder("typeDescriptor", thisTypeDescriptor)
+                .addModifiers(KModifier.OVERRIDE)
+                .getter(
+                    FunSpec.getterBuilder()
+                        .addStatement("return Companion")
+                        .build()
+                )
+                .build()
+        )
+
+
+        val implType = TypeSpec.classBuilder("Impl")
+        implType.addSuperinterface(thisCname)
+        if (handleType.parent != null) {
+            val parentVariableName = handleType.parent.variableName()
+            val parentCname = handleType.parent.className()
+            implType.addProperty(
+                PropertySpec.builder(parentVariableName, parentCname)
+                    .addModifiers(KModifier.OVERRIDE)
+                    .initializer(CodeBlock.of(parentVariableName))
+                    .build()
+            )
+            implType.addProperty(
+                PropertySpec.builder("handle", CBasicType.int64_t.kotlinTypeName)
+                    .addModifiers(KModifier.OVERRIDE)
+                    .initializer(CodeBlock.of("handle"))
+                    .build()
+            )
+            implType.primaryConstructor(
+                FunSpec.constructorBuilder()
+                    .addParameter(parentVariableName, handleType.parent.className())
+                    .addParameter("handle", CBasicType.int64_t.kotlinTypeName)
+                    .build()
+            )
+            val grandparent = handleType.parent.parent
+            if (grandparent != null) {
+                implType.addSuperinterface(
+                    grandparent.className().nestedClass("Child"),
+                    CodeBlock.of(parentVariableName)
+                )
+            }
+        } else {
+            implType.addSuperclassConstructorParameter(
+                CodeBlock.of("%M", CBasicType.int64_t.valueLayoutMember)
+            )
+            implType.addProperty(
+                PropertySpec.builder("handle", CBasicType.int64_t.kotlinTypeName)
+                    .addModifiers(KModifier.OVERRIDE)
+                    .initializer(CodeBlock.of("handle"))
+                    .build()
+            )
+            implType.primaryConstructor(
+                FunSpec.constructorBuilder()
+                    .addParameter("handle", CBasicType.int64_t.kotlinTypeName)
+                    .build()
+            )
+        }
+        interfaceType.addType(implType.build())
+
+
+        val childType = TypeSpec.interfaceBuilder("Child")
+        handleType.parent?.className()?.nestedClass("Child")?.let {
+            childType.addSuperinterface(it)
+        }
+        childType.addProperty(handleType.variableName(), thisCname)
+        interfaceType.addType(childType.build())
+
+
+        val companion = TypeSpec.companionObjectBuilder()
+        companion.superclass(vkTypeDescriptorCname.parameterizedBy(thisCname))
+        companion.addFunction(
+            FunSpec.builder("toNativeData")
+                .addAnnotation(JvmStatic::class)
+                .addModifiers(KModifier.INLINE)
+                .addParameter("value", thisCname)
+                .returns(CBasicType.int64_t.kotlinTypeName)
+                .addStatement("return value.handle")
+                .build()
+        )
+        companion.addFunction(
+            FunSpec.builder("fromNativeData")
+                .addAnnotation(JvmStatic::class)
+                .addModifiers(KModifier.INLINE)
+                .addParameter("value", CBasicType.int64_t.kotlinTypeName)
+                .returns(thisCname)
+                .apply {
+                    if (handleType.parent == null) {
+                        addStatement("return Impl(value)")
+                    } else {
+                        addStatement(
+                            "throw %T()",
+                            UnsupportedOperationException::class
+                        )
+                    }
+                }
+                .build()
+        )
+        companion.addMethodHandleFields()
+        interfaceType.addType(companion.build())
+
+
+        val file = FileSpec.builder(thisCname)
+        file.addType(interfaceType.build())
+        return file
     }
 }
