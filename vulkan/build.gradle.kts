cff9cb7cbd5ef17c408f22814639a0c7031b134c
xiguajerry <xiguajerry@outlook.com>
2025-04-17 01:42:16 -0400

    update Test.kt
    (cherry picked from commit 2eebedebfc41d2ba699ef43c5f3f6659cde6e6aa)

--- vulkan/build.gradle.kts
+++ vulkan/build.gradle.kts
@@ -2,10 +2,19 @@ plugins {
     id("net.echonolix.ktgen")
 }
 
+val lwjglVersion = "3.3.6"
+val lwjglNatives = "natives-windows"
+
 dependencies {
     ktgen(project(":vulkan:codegen"))
     implementation(kotlin("reflect"))
     api(project(":ktffi-core"))
+
+    testImplementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    testImplementation("org.lwjgl", "lwjgl", lwjglVersion)
+    testImplementation("org.lwjgl", "lwjgl-glfw", lwjglVersion)
+    testRuntimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    testRuntimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
 }
 
 kotlin {
--- vulkan/src/test/kotlin/net/echonolix/vulkan/Test.kt
+++ vulkan/src/test/kotlin/net/echonolix/vulkan/Test.kt
@@ -1,9 +1,26 @@
 package net.echonolix.vulkan
 
 import net.echonolix.ktffi.*
+import net.echonolix.vulkan.Vk.vkCreateInstance
+import net.echonolix.vulkan.enums.VkPhysicalDeviceType
 import net.echonolix.vulkan.flags.VkDebugUtilsMessageSeverityFlagsEXT
 import net.echonolix.vulkan.flags.VkDebugUtilsMessageTypeFlagsEXT
+import net.echonolix.vulkan.flags.VkQueueFlags
+import net.echonolix.vulkan.handles.VkInstance
+import net.echonolix.vulkan.handles.VkPhysicalDevice
+import net.echonolix.vulkan.handles.get
+import net.echonolix.vulkan.handles.value
 import net.echonolix.vulkan.structs.*
+import org.lwjgl.glfw.GLFW.GLFW_CLIENT_API
+import org.lwjgl.glfw.GLFW.GLFW_FALSE
+import org.lwjgl.glfw.GLFW.GLFW_NO_API
+import org.lwjgl.glfw.GLFW.GLFW_RESIZABLE
+import org.lwjgl.glfw.GLFW.glfwCreateWindow
+import org.lwjgl.glfw.GLFW.glfwInit
+import org.lwjgl.glfw.GLFW.glfwTerminate
+import org.lwjgl.glfw.GLFW.glfwWindowHint
+import org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions
+import org.lwjgl.system.MemoryUtil
 
 private fun populateDebugMessengerCreateInfo(debugCreateInfo: NativeValue<VkDebugUtilsMessengerCreateInfoEXT>) {
     debugCreateInfo.messageSeverity = VkDebugUtilsMessageSeverityFlagsEXT.VERBOSE_EXT +
@@ -15,39 +32,117 @@ private fun populateDebugMessengerCreateInfo(debugCreateInfo: NativeValue<VkDebu
         VkDebugUtilsMessageTypeFlagsEXT.PERFORMANCE_EXT
 
     debugCreateInfo.pfnUserCallback { messageSeverity, messageType, pCallbackData, pUserData ->
-        if (VkDebugUtilsMessageSeverityFlagsEXT.ERROR_EXT in messageSeverity) {
-            System.err.println("Validation layer: " + pCallbackData.pMessage.string)
-        } else {
-            println("Validation layer: " + pCallbackData.pMessage.string)
-        }
+        System.err.println("Validation layer: " + pCallbackData.pMessage.string)
         VK_FALSE
     }
 }
 
 fun main() {
     MemoryStack {
+        // region Init GLFW
+        glfwInit()
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API)
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE)
+        val width = 800
+        val height = 600
+        glfwCreateWindow(width, height, "Vulkan", 0, 0)
+        // endregion
+
         val layers = setOf("VK_LAYER_KHRONOS_validation")
-        val extensions = setOf("VK_EXT_debug_utils", "VK_KHR_surface")
+        val extensions = buildSet {
+            val buffer = glfwGetRequiredInstanceExtensions() ?: return@buildSet
+            repeat(buffer.capacity()) { add(MemoryUtil.memASCII(buffer.get(it))) }
+        } + setOf(VK_EXT_DEBUG_UTILS_EXTENSION_NAME)
+        println(extensions)
 
-        val appInfo = VkApplicationInfo.allocate()
-        appInfo.pApplicationName = "Hello Vulkan".c_str()
-        appInfo.applicationVersion = VkApiVersion(0u, 1u, 0u, 0u).value
-        appInfo.pEngineName = "Echonolix".c_str()
-        appInfo.engineVersion = VkApiVersion(0u, 1u, 0u, 0u).value
-        appInfo.apiVersion = VK_API_VERSION_1_0.value
+        val appInfo = VkApplicationInfo.allocate().apply {
+            pApplicationName = "Hello Vulkan".c_str()
+            applicationVersion = VkApiVersion(0u, 1u, 0u, 0u).value
+            pEngineName = "Echonolix".c_str()
+            engineVersion = VkApiVersion(0u, 1u, 0u, 0u).value
+            apiVersion = VK_API_VERSION_1_0.value
+        }
 
-        val createInfo = VkInstanceCreateInfo.allocate()
-        createInfo.pApplicationInfo = appInfo.ptr()
-
-        createInfo.enabledExtensionCount = extensions.size.toUInt()
-        createInfo.ppEnabledExtensionNames = extensions.c_strs()
-        createInfo.enabledLayerCount = 1u
-        createInfo.ppEnabledLayerNames = layers.c_strs()
+        val createInfo = VkInstanceCreateInfo.allocate().apply {
+            pApplicationInfo = appInfo.ptr()
+            ppEnabledExtensionNames = extensions.c_strs()
+            enabledExtensionCount = extensions.size.toUInt()
+            ppEnabledLayerNames = layers.c_strs()
+            enabledLayerCount = layers.size.toUInt()
+        }
         val debugCreateInfo = VkDebugUtilsMessengerCreateInfoEXT.allocate()
         populateDebugMessengerCreateInfo(debugCreateInfo)
-        createInfo.pNext = debugCreateInfo.ptr()
 
-        val instance = Vk.createInstance(createInfo.ptr(), null).getOrThrow()
-        instance.vkDestroyInstance(instance, null)
+        val instancePtr = VkInstance.malloc()
+        val result = vkCreateInstance(createInfo.ptr(), nullptr(), instancePtr.ptr())
+        println(result.name)
+        val instance = VkInstance.fromNativeData(instancePtr.value)
+        val debugUtilsMessenger = instance.createDebugUtilsMessengerEXT(debugCreateInfo.ptr(), null).getOrThrow()
+
+        var physicalDeviceHandle = -1L
+        run {
+            val physicalDeviceCount = NativeUInt32.calloc()
+            instance.enumeratePhysicalDevices(physicalDeviceCount.ptr(), null)
+            require(physicalDeviceCount.value != 0u) { "No physical device found." }
+            val physicalDevices = VkPhysicalDevice.malloc(physicalDeviceCount.value)
+            instance.enumeratePhysicalDevices(physicalDeviceCount.ptr(), physicalDevices.ptr())
+            repeat(physicalDeviceCount.value.toInt()) {
+                val property = VkPhysicalDeviceProperties.allocate()
+                val device = VkPhysicalDevice.fromNativeData(physicalDevices[it])
+                device.getPhysicalDeviceProperties(property.ptr())
+                if (property.deviceType == VkPhysicalDeviceType.DISCRETE_GPU) physicalDeviceHandle = device.handle
+            }
+        }
+        check(physicalDeviceHandle != -1L) { "No suitable physical device found." }
+        val physicalDeviceProperties = VkPhysicalDeviceProperties.allocate()
+        val physicalDeviceFeatures = VkPhysicalDeviceFeatures.allocate()
+        val physicalDevice = VkPhysicalDevice.fromNativeData(physicalDeviceHandle)
+        physicalDevice.getPhysicalDeviceProperties(physicalDeviceProperties.ptr())
+        physicalDevice.getPhysicalDeviceFeatures(physicalDeviceFeatures.ptr())
+
+        var graphicsQueueIndex = -1
+        run {
+            val queueFamilyPropertyCount = NativeUInt32.calloc()
+            physicalDevice.getPhysicalDeviceQueueFamilyProperties(queueFamilyPropertyCount.ptr(), null)
+            val queueFamilyProperties = VkQueueFamilyProperties.allocate(queueFamilyPropertyCount.value)
+            physicalDevice.getPhysicalDeviceQueueFamilyProperties(
+                queueFamilyPropertyCount.ptr(),
+                queueFamilyProperties.ptr()
+            )
+            repeat(queueFamilyPropertyCount.value.toInt()) {
+                val queueFamilyProperty = queueFamilyProperties[it.toLong()]
+                if (queueFamilyProperty.queueFlags.contains(VkQueueFlags.VK_QUEUE_GRAPHICS_BIT)
+                    && graphicsQueueIndex == -1
+                ) {
+                    graphicsQueueIndex = it
+                }
+            }
+        }
+
+        val graphicsQueuePriority = NativeFloat.calloc().apply { value = 1f }
+        val graphicsQueueCreateInfo = VkDeviceQueueCreateInfo.allocate().apply {
+            queueFamilyIndex = graphicsQueueIndex.toUInt()
+            queueCount = 1u
+            pQueuePriorities = graphicsQueuePriority.ptr()
+        }
+
+        val deviceCreateInfo = VkDeviceCreateInfo.allocate().apply {
+            pQueueCreateInfos = graphicsQueueCreateInfo.ptr()
+            queueCreateInfoCount = 1u
+            pEnabledFeatures = physicalDeviceFeatures.ptr()
+            // TODO: Add extensions
+            enabledExtensionCount = 0u
+
+            enabledLayerCount = layers.size.toUInt()
+            ppEnabledLayerNames = layers.c_strs()
+        }
+
+        val device = physicalDevice.createDevice(deviceCreateInfo.ptr(), null).getOrThrow()
+
+        device.destroyDevice(null)
+        instance.destroyDebugUtilsMessengerEXT(debugUtilsMessenger, null)
+        instance.destroyInstance(null)
+
+        glfwTerminate()
     }
 }
